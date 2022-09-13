7143a39feb7f77d5af75505748116ffcf5b27eb7
Gonzalo Hernandez <Hernandezgonzalo584@gmail.com>
2022-09-13 02:37:15 -0300

    ADDEDResponsive

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//SELECCIONAR CONTENIDO DEL HTML:
+
+//querySelector: 
+const headingTitulo = document.querySelector(".encabezado_titulo"); //RETORNA 0 O 1 ELEMENTO
+//es decir, si hay mas de 1 elemento igual, retorna solo 1.
+console.log(headingTitulo);
+
+//headingTitulo.textContent = 'Cambie el heading'; //CAMBIO EL CONTENIDO DEL HEADING
+
+//headingTitulo.classList = 'Agrego-clase' //Agrego una clase mas al heading
+
+
+
+//querySelectorAll
+
+
+
+//getElementById
