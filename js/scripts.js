0a7cb0ae3dea3510630b473b518dd9db7a317c31
Gonzalo Hernandez <Hernandezgonzalo584@gmail.com>
2022-09-13 16:05:05 -0300

    ADDEDAlertContact

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
+
+
+
 //SELECCIONAR CONTENIDO DEL HTML:
 
 //querySelector: 
-const headingTitulo = document.querySelector(".encabezado_titulo"); //RETORNA 0 O 1 ELEMENTO
-//es decir, si hay mas de 1 elemento igual, retorna solo 1.
-console.log(headingTitulo);
 
 //headingTitulo.textContent = 'Cambie el heading'; //CAMBIO EL CONTENIDO DEL HEADING
 
@@ -13,6 +13,98 @@ console.log(headingTitulo);
 
 //querySelectorAll
 
-
-
 //getElementById
+
+//Validar FORMULARIO
+
+const datos ={
+    nombreForm:' ',
+    telForm:'',
+    mailForm:'',
+    msgForm:''
+}
+
+const nombreForm = document.querySelector('#nombreForm');
+nombreForm.addEventListener('input',leerInput);
+const telForm  = document.querySelector('#telForm');
+
+telForm.addEventListener('input',leerInput);
+const mailForm = document.querySelector('#mailForm');
+
+mailForm.addEventListener('input',leerInput);
+
+const msgForm = document.querySelector('#msgForm');
+
+msgForm.addEventListener('input',leerInput);
+
+function leerInput(e){
+    
+
+    datos[e.target.id] = e.target.value;
+    
+
+}
+
+
+
+
+
+const form = document.querySelector(".form-contacto");
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const{nombreForm,telForm,mailForm,msgForm}  = datos;
+    
+  
+    if(nombreForm == '' || telForm == ''|| mailForm=='' || msgForm==''){
+    
+      
+
+        mostrarAlerta('TODOS LOS CAMPOS SON OBLIGATORIOS','error');
+
+    }
+    else{
+
+        mostrarAlerta('ENVIADO CORRECTAMENTE','correcto');
+        
+    }
+
+})
+
+
+function mostrarAlerta(alerta,estado){
+
+    const encabezadoTexto = document.querySelector('.encabezado__texto');
+   
+
+    const alertah4 = document.createElement('H4');
+
+    alertah4.textContent = alerta;
+   
+
+    if(estado=='error'){
+
+        alertah4.classList.add('error');
+
+    }
+    else{
+        alertah4.classList.add('correcto');
+
+       
+    }
+
+
+
+    form.appendChild(alertah4);
+
+    setTimeout(()=>{
+        alertah4.remove();   
+       },5000);
+
+
+    
+
+
+
+
+}
+
